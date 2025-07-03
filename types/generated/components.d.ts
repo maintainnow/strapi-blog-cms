81d2b8bbb32953b0269a0934a25a049b65f3eb08
iniaarsen <iniaarsen@gmail.com>
2025-07-03 15:49:31 -0700

    refactor: update rich text component to use blocks attribute type

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -31,7 +31,7 @@ export interface SharedRichText extends Struct.ComponentSchema {
     icon: 'align-justify';
   };
   attributes: {
-    body: Schema.Attribute.RichText;
+    body: Schema.Attribute.Blocks;
   };
 }
 
